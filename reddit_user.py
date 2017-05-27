c97e90e7f1fe5bf6e65c0a86fd6d6143fcc8ed9d
yzhao <happyzychjl@gmail.com>
2017-05-27 14:38:55 -0400

    Integrated web app

--- reddit_user.py
+++ reddit_user.py
@@ -15,6 +15,22 @@ class User(object):
     def comments(self):
         return self._user.comments.new(limit=None)
 
+    @staticmethod
+    def rate_of_negativeness(username):
+        ucomment = User(username)
+        sent = Sentiment()
+        sum_t = 0
+        sum_of_negative_comments = 0
+        for comment in ucomment.comments:
+            sum_t += 1
+            if comment.score < 0:
+                sum_of_negative_comments += 1
+                # print(comment.body)
+                # print(sent.get_sentiment(comment.body, comment.score))
+                # print("Score %d" % comment.score)
+                # print("Score weighted %f" % Sentiment.score_func(comment.score))
+                # print("Score nltk %f" % sent.get_compound())
+        return 1.0*sum_of_negative_comments/sum_t
 
 def test():
     reddit = praw.Reddit(client_id=config.app_id,
@@ -31,13 +47,6 @@ if __name__ == '__main__':
     test_users = ['ghostcheck', 'ViaGamma', 'rogueqd']
     # test()
     # ucomment = User(test_users[2])
-    ucomment = User('the-realDonaldTrump')
-    sent = Sentiment()
-    for comment in ucomment.comments:
-        if comment.score < 0:
-            print(comment.body)
-            print(sent.get_sentiment(comment.body, comment.score))
-            print("Score %d" % comment.score)
-            print("Score weighted %f" % Sentiment.score_func(comment.score))
-            print("Score nltk %f" % sent.get_compound())
+    # User.rate_of_negativeness('the-realDonaldTrump')
+    print(User.rate_of_negativeness(test_users[2]))
 
