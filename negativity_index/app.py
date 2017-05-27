8fbe2f54dad082ae508fb6f5f677ec414e98994c
Emily Zeng <zhixuan.zeng@gmail.com>
2017-05-27 15:57:45 -0400

    changed UserID to username

--- negativity_index/app.py
+++ negativity_index/app.py
@@ -2,25 +2,26 @@ from flask import Flask, render_template, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
+from .reddit_user import User
 
 
 app = Flask(__name__)
 app.config.from_object('config')
 
 class IDForm(FlaskForm):
-    reddit = StringField('UserID', validators=[DataRequired()])
+    reddit = StringField('username', validators=[DataRequired()])
 
 @app.route('/', methods=['GET', 'POST'])
-def redditID():
+def reddit_username():
     form = IDForm()
     if form.is_submitted():
-        return redirect(url_for('assholeIndex', userID=form.reddit.data))
+        return redirect(url_for('assholeIndex', username=form.reddit.data))
     return render_template('index.html',
-                           title='enter UserID',
+                           title='enter username',
                            form=form)
 
-@app.route('/<userID>', methods=['GET', 'POST'])
-def assholeIndex(userID=None):
-    rate = reddit_user.User.rate_of_negativeness(userID)
+@app.route('/<username>', methods=['GET', 'POST'])
+def assholeIndex(username=None):
+    rate = User.rate_of_negativeness(username)
     return render_template('asshole_index.html',
-                           userID=userID, rateOfNegativeness=rate)
+                           username=username, rateOfNegativeness=rate)
